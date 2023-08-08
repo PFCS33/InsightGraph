ea8e55ebfed7f0f8df9010ad5a9a9ea1aef43434
pfcs33 <aaapfcs@gmail.com>
2023-08-09 00:33:46 +0800

    adding dist

---
+++ data/test_data/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+ # lastChildren = focus_cell['children'][-1]
+                        # lastChildren['rowSpan'] = [
+                        #     lastChildren['children'][0]['rowSpan'][0], lastChildren['children'][-1]['rowSpan'][0]]
+                        # lastSbiling = row_info[-1]
+                        # lastSbiling['rowSpan'] = [
+                        #     lastSbiling['children'][0]['rowSpan'][0], lastSbiling['children'][-1]['rowSpan'][0]]
