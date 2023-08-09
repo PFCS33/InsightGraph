81152fdf20dfe62af2dd69dca0afa692287636c3
pfcs33 <aaapfcs@gmail.com>
2023-08-09 15:32:46 +0800

    add data

---
+++ src/components/control-panel-graph/MiniTable.vue
@@ -0,0 +1,296 @@
+<template>
+  <div id="table-container"></div>
+</template>
+<script>
+import { flatAccessWithDatum } from "vega-lite";
+
+export default {
+  data() {
+    return {
+      rowName: null,
+      colName: null,
+      rowDepth: null,
+      colDepth: null,
+      rowNum: null,
+      colNum: null,
+      rowDict: null,
+      colDict: null,
+    };
+  },
+  methods: {
+    createTable() {
+      const that = this;
+      const container = d3.select("#table-container");
+
+      const tooltip = container
+        .append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+      const table = container
+        .append("table")
+        .attr("class", "mini-table")
+        .style("user-select", "none");
+
+      const width = parseInt(container.style("width"), 10);
+      const height = parseInt(container.style("height"), 10);
+
+      const cellWidth = width / (this.colNum + this.rowDepth);
+      const cellHeight = height / (this.rowNum + this.colDepth);
+
+      // Convert 'rows' and 'cols' to a dictionary
+
+      // Create table headers
+      const thead = table.append("thead");
+      for (let i = 0; i < this.colDepth; i++) {
+        thead.append("tr");
+      }
+
+      const tbody = table.append("tbody");
+
+      thead
+        .select("tr")
+        .append("th")
+        .attr("colspan", this.rowDepth)
+        .attr("rowspan", this.colDepth)
+        .attr("width", cellWidth)
+        .attr("height", cellHeight);
+
+      this.tableData.cols.forEach((colCell) => {
+        addTopCell(colCell, thead, 1, tooltip);
+      });
+
+      const tr = tbody.append("tr");
+      this.tableData.rows.forEach((rowCell) => {
+        addLeftCell(rowCell);
+      });
+
+      table
+        .selectAll("th")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseleave", mouseleave);
+      function mouseover(event, d) {
+        if (d) {
+          tooltip.style("opacity", 1);
+          d3.select(this).classed("cell-border-tooltip-highlight", true);
+        }
+      }
+      function mousemove(event, d) {
+        if (d) {
+          tooltip
+            .html(d.name)
+            .style("left", event.x + "px")
+            .style("top", event.y - 30 + "px");
+        }
+      }
+      function mouseleave(event, d) {
+        if (d) {
+          tooltip.style("opacity", 0);
+          d3.select(this).classed("cell-border-tooltip-highlight", false);
+        }
+      }
+
+      function addLeftCell(cell) {
+        const tr = tbody.selectAll("tr").filter(function (d, i, nodes) {
+          return i === nodes.length - 1;
+        });
+        tr.append("th")
+          .data([cell])
+          .attr("rowspan", cell.rowSpan[1] - cell.rowSpan[0] + 1)
+          .attr("width", cellWidth)
+          .attr("height", cellHeight);
+        if (cell.children) {
+          cell.children.forEach((cell) => {
+            addLeftCell(cell);
+          });
+        } else {
+          for (let i = 0; i < that.colNum; i++) {
+            tr.append("td").attr("width", cellWidth).attr("height", cellHeight);
+          }
+          tbody.append("tr");
+        }
+      }
+
+      function addTopCell(cell, thead, depth) {
+        const tr = thead.select(`tr:nth-child(${depth})`);
+        tr.append("th")
+          .data([cell])
+          .attr("colspan", cell.colSpan[1] - cell.colSpan[0] + 1)
+          .attr("width", cellWidth)
+          .attr("height", cellHeight);
+        if (cell.children) {
+          cell.children.forEach((cell) => {
+            addTopCell(cell, thead, depth + 1);
+          });
+        }
+      }
+    },
+
+    addHighlightBackground(checkedArea, mode) {
+      if (checkedArea) {
+        const tbody = d3
+          .select("#table-container")
+          .select("table")
+          .select("tbody");
+        for (let [key, value] of checkedArea) {
+          const rowSpan = value.rowSpan;
+          const colSpan = value.colSpan;
+          const trGroup = tbody.selectChildren("tr").filter(function (d, i) {
+            return i >= rowSpan[0] && i <= rowSpan[1];
+          });
+          trGroup
+            .selectChildren("td")
+            .filter(function (d, i) {
+              return i >= colSpan[0] && i <= colSpan[1];
+            })
+            .classed("cell-background-highlight", mode);
+        }
+      }
+    },
+    addHighlightBorder(checkedArea, mode) {
+      if (checkedArea) {
+        const tbody = d3
+          .select("#table-container")
+          .select("table")
+          .select("tbody");
+        for (let [key, value] of checkedArea) {
+          const rowSpan = value.rowSpan;
+          const colSpan = value.colSpan;
+          const trGroup = tbody.selectChildren("tr").filter(function (d, i) {
+            return i >= rowSpan[0] && i <= rowSpan[1];
+          });
+          const tdGroup = trGroup.selectChildren("td").filter(function (d, i) {
+            return i >= colSpan[0] && i <= colSpan[1];
+          });
+
+          addHorizontalBorder(colSpan, tdGroup);
+          addVerticalBorder(colSpan, rowSpan, trGroup);
+        }
+      }
+
+      function addHorizontalBorder(colSpan, tdGroup) {
+        const colWidth = colSpan[1] - colSpan[0];
+
+        tdGroup.each(function (d, i) {
+          // 'this' is the current group
+          if (i == 0)
+            d3.select(this).classed("cell-border-left-highlight", mode);
+          if (i === colWidth)
+            d3.select(this).classed("cell-border-right-highlight", mode);
+        });
+      }
+      function addVerticalBorder(colSpan, rowSpan, trGroup) {
+        const rowWidth = rowSpan[1] - rowSpan[0];
+
+        trGroup.each(function (d, i) {
+          if (i == 0) {
+            d3.select(this)
+              .selectAll("td")
+              .filter(function (d, i) {
+                return i >= colSpan[0] && i <= colSpan[1];
+              })
+              .classed("cell-border-top-highlight", mode);
+          } else if (i === rowWidth) {
+            d3.select(this)
+              .selectAll("td")
+              .filter(function (d, i) {
+                return i >= colSpan[0] && i <= colSpan[1];
+              })
+              .classed("cell-border-bottom-highlight", mode);
+          }
+        });
+      }
+    },
+  },
+  computed: {
+    tableData() {
+      return this.$store.getters["table/tableData"];
+    },
+    checkedArea() {
+      return this.$store.getters["table/checkedArea"];
+    },
+    hoveredArea() {
+      return this.$store.getters["table/hoveredArea"];
+    },
+  },
+  watch: {
+    checkedArea: {
+      handler(newVal, oldVal) {
+        this.addHighlightBorder(oldVal, false);
+        this.addHighlightBorder(newVal, true);
+      },
+      deep: true,
+    },
+    hoveredArea: {
+      handler(newVal, oldVal) {
+        this.addHighlightBackground(oldVal, false);
+        this.addHighlightBackground(newVal, true);
+      },
+      deep: true,
+    },
+  },
+  mounted() {
+    const data = this.tableData;
+    this.rowNum = data.rowNum;
+    this.colNum = data.colNum;
+    this.rowDepth = data.rowDepth;
+    this.colDepth = data.colDepth;
+
+    this.createTable();
+    this.addHighlightBorder(this.checkedArea, true);
+  },
+};
+</script>
+<style scoped>
+#table-container {
+  width: 100%;
+  height: 100%;
+}
+</style>
+<style lang="less">
+table.mini-table {
+  //   border-collapse: collapse;
+  border-spacing: 0;
+  border-collapse: separate;
+  border: 1.25px solid #555;
+}
+
+table.mini-table {
+  th {
+    border: 1.25px solid #555;
+  }
+  td {
+    border: 1px dashed #ccc;
+  }
+}
+.tooltip {
+  background-color: #fff;
+  border: 2px solid #555;
+  border-radius: 5px;
+  padding: 5px;
+  width: fit-content;
+  height: fit-content;
+  position: fixed;
+}
+th.cell-border-tooltip-highlight {
+  border-width: 2px !important;
+  background-color: #ddd;
+}
+td.cell-border-left-highlight {
+  border-left: 2px solid #000 !important;
+}
+td.cell-border-right-highlight {
+  border-right: 2px solid #000 !important;
+}
+
+td.cell-border-top-highlight {
+  border-top: 2px solid #000 !important;
+}
+td.cell-border-bottom-highlight {
+  border-bottom: 2px solid #000 !important;
+}
+td.cell-background-highlight {
+  background-color: #ddd;
+}
+</style>
---
+++ src/store/modules/table.js
@@ -0,0 +1,127 @@
+import { resetTitleFormatter } from "vega-lite/build/src/channeldef";
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      tableData: null,
+      rowDict: null,
+      colDict: null,
+      checkedArea: null,
+      hoveredArea: null,
+    };
+  },
+  getters: {
+    tableData(state) {
+      return state.tableData;
+    },
+    rowDict(state) {
+      return state.rowDict;
+    },
+    colDict(state) {
+      return state.colDict;
+    },
+    checkedArea(state) {
+      return state.checkedArea;
+    },
+    hoveredArea(state) {
+      return state.hoveredArea;
+    },
+  },
+  mutations: {
+    setTableData(state, payload) {
+      state.tableData = payload;
+    },
+    setRowDict(state, payload) {
+      state.rowDict = payload;
+    },
+    setColDict(state, payload) {
+      state.colDict = payload;
+    },
+    setCheckedArea(state, payload) {
+      state.checkedArea = payload;
+    },
+    setHoveredArea(state, payload) {
+      console.log(payload);
+      state.hoveredArea = payload;
+    },
+  },
+  actions: {
+    convertCheckSelection(context, payload) {
+      let result = new Map();
+      const data = payload.data;
+      const mode = payload.mode;
+      if (data) {
+        for (let [key, value] of data.entries()) {
+          const rowName = value.row;
+          const colName = value.col;
+          const rowDict = context.getters.rowDict;
+          const colDict = context.getters.colDict;
+
+          let colSpan = null;
+          if (colName === "_") {
+            colSpan = [0, context.getters.tableData.colNum - 1];
+          } else {
+            colSpan = findColSpan(colName, colDict);
+          }
+          let rowSpan = null;
+          if (rowName === "_") {
+            rowSpan = [0, context.getters.tableData.rowNum - 1];
+          } else {
+            rowSpan = findRowSpan(rowName, rowDict);
+          }
+
+          result.set(key, {
+            rowSpan: rowSpan,
+            colSpan: colSpan,
+          });
+        }
+      } else {
+        result = null;
+      }
+
+      if (mode === "checked") context.commit("setCheckedArea", result);
+      else if (mode === "hovered") context.commit("setHoveredArea", result);
+
+      function findColSpan(colName, colDict) {
+        const path = colName.split("_");
+        let info = colDict[path[0]];
+        for (let i = 1; i < path.length; i++) {
+          info = info.children[path[i]];
+        }
+        return info.colSpan;
+      }
+      function findRowSpan(rowName, rowDict) {
+        const path = rowName.split("_");
+        let info = rowDict[path[0]];
+        for (let i = 1; i < path.length; i++) {
+          info = info.children[path[i]];
+        }
+        return info.rowSpan;
+      }
+    },
+
+    loadHeadData(context, payload) {
+      const path = "data/test_data/table.json";
+      d3.json(path).then(function (data) {
+        context.commit("setTableData", data.table);
+        context.commit("setRowDict", listToDict(data.table.rows));
+        context.commit("setColDict", listToDict(data.table.cols));
+      });
+
+      function listToDict(list) {
+        let obj = {};
+        for (let item of list) {
+          if ("children" in item && item.children) {
+            // Don't modify the existing list, but create a new dict for children
+            let childObj = listToDict(item.children);
+            obj[item.name] = { ...item, children: childObj };
+          } else {
+            obj[item.name] = item;
+          }
+        }
+        return obj;
+      }
+    },
+  },
+};
